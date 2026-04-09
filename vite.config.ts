8c1e7b3c678be10c660adbdf792e766911da5db8
acheCoder <hubertchim.97@gmail.com>
2026-04-09 11:11:07 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: 'app-host',
+      // Aquí es donde en el futuro listaremos los microfrontales remotos
+      remotes: {
+        // Ejemplo: remoteClientes: "http://localhost:5001/assets/remoteEntry.js",
+      },
+      // Compartimos nuestras librerías base para que los remotos no las vuelvan a cargar
+      shared: ['react', 'react-dom', 'react-redux', '@reduxjs/toolkit'],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
